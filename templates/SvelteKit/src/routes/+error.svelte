02c481795ef93cc63415b681d4e373ced4aacb16
babakfp <babak.bxf@gmail.com>
2025-04-30 19:46:06 +0330

    add default +error.svelte

---
+++ templates/SvelteKit/src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { page } from "$app/state"
+</script>
+
+<main
+    class="container grid min-h-svh content-center items-center gap-2 py-8 text-center"
+>
+    <h1 class="text-4xl font-black">{page.status}</h1>
+    {#if page.error?.message}
+        <p>{page.error.message}</p>
+    {/if}
+</main>
