884085b2d50c57adae5f260156236120770ae408
babakfp <babak.bxf@gmail.com>
2025-04-30 19:48:59 +0330

    use text-7xl for title in error pages

--- templates/SvelteKit/src/routes/+error.svelte
+++ templates/SvelteKit/src/routes/+error.svelte
@@ -5,7 +5,7 @@
 <main
     class="container grid min-h-svh content-center items-center gap-2 py-8 text-center"
 >
-    <h1 class="text-4xl font-black">{page.status}</h1>
+    <h1 class="text-7xl font-black">{page.status}</h1>
     {#if page.error?.message}
         <p>{page.error.message}</p>
     {/if}
